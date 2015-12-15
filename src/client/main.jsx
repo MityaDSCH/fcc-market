4010625317b67a2f8b0f028edb5d5d1f87d0ce59
MityaDSCH <mlsgearon@gmail.com>
2015-12-15 15:24:38 -0600

    wait, why did I have a router?

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -2,14 +2,9 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute } from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 import App from './components/app.jsx';
 
 render((
-  <Router history={createBrowserHistory()}>
-    <Route path="/" component={App}>
-    </Route>
-  </Router>
+  <App />
 ), document.getElementById('app'));
